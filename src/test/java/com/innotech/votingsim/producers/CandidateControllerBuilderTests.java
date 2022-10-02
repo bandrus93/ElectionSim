0959bfdc954451c0ea9353e79bfe2335a21316c4
Ben Andrus <bjandrus93@gmail.com>
2022-10-01 22:31:01 -0400

    Added more input tests.

--- src/test/java/com/innotech/votingsim/producers/CandidateControllerBuilderTests.java
+++ src/test/java/com/innotech/votingsim/producers/CandidateControllerBuilderTests.java
@@ -1,18 +1,50 @@
 package com.innotech.votingsim.producers;
 
+import com.innotech.votingsim.inputs.ActionInput;
+import com.innotech.votingsim.inputs.NumeralInput;
+import com.innotech.votingsim.inputs.SpinnerInput;
 import com.innotech.votingsim.inputs.TextInput;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
 
 public class CandidateControllerBuilderTests {
+    private CandidateControllerBuilder testBuilder;
 
-    @Test
+    @BeforeGroups({"inputTests"})
+    public void setupInputTests() {
+        testBuilder = new CandidateControllerBuilder();
+    }
+
+    @Test(groups = {"inputTests"})
     public void addInputShouldSetNameFieldGivenATextInputWithLabelOfName() {
-        CandidateControllerBuilder testBuilder = new CandidateControllerBuilder();
         TextInput testInput = new TextInput("Name:");
         testBuilder.addInput(testInput);
         assertEquals(testBuilder.build().getNameField(), testInput);
+    }
+
+    @Test(groups = {"inputTests"})
+    public void addInputShouldSetEnergyFieldGivenANumeralInputWithALabelOfEnergy() {
+        NumeralInput testInput = new NumeralInput("Energy:");
+        testBuilder.addInput(testInput);
+        assertEquals(testBuilder.build().getEnergyField(), testInput);
+    }
+
+    @Test(groups = {"inputTests"})
+    public void addInputShouldSetAllySelectorGivenASpinnerInputWithALabelOfAlignment() {
+        SpinnerInput testInput = new SpinnerInput("Alignment:", Arrays.asList("Radical Left", "Moderate Left", "Center", "Moderate Right", "Radical Right").toArray());
+        testBuilder.addInput(testInput);
+        assertEquals(testBuilder.build().getAllySelector(), testInput);
+    }
+
+    @Test(groups = {"inputTests"})
+    public void addInputShouldSetAddCandidateButtonGivenAnActionInputWithALabelOfAddCandidate() {
+        ActionInput testInput = new ActionInput("Add Candidate");
+        testBuilder.addInput(testInput);
+        assertEquals(testBuilder.build().getAddInput(), testInput);
     }
 
 }
